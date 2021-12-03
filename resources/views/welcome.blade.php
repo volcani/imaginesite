a1e068700dabc8de0f190dfb55679333147c7ea1
neko <catoulthar@gmail.com>
2021-12-04 02:43:24 +0900

    ログインをコメントアウト

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,7 @@
         </style>
     </head>
     <body>
+        {{--
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -76,6 +77,7 @@
                     @endauth
                 </div>
             @endif
+        --}}
 
             <div class="content">
                 <div class="title m-b-md">
