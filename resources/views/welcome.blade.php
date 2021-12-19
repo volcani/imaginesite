4941b9896ccd63edb69afa563be182998a0d6cb9
neko <catoulthar@gmail.com>
2021-12-20 02:20:27 +0900

    メニュー

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel全くわからんぞ
                 </div>
 
                 <div class="links">
